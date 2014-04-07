4e45f59e187bf366f52ced4fdc2e36678a013031
Erez Zukerman <bulk@ezuk.org>
2014-04-07 20:30:46 +0300

    made icon, limited permissions, fixed visiblity

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,14 +1,18 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-    console.log('previousVersion', details.previousVersion);
-});
-
-chrome.tabs.onUpdated.addListener(function (tabId) {
-    chrome.pageAction.show(tabId);
-});
-
 chrome.pageAction.onClicked.addListener(function(tab) {
-  console.log('Turning ' + tab.url + ' red!');
   chrome.tabs.executeScript(null, {file: "scripts/lint_post.js"});
 });
+
+function onWebNav(details) {
+    if (details.frameId === 0) {
+        chrome.pageAction.show(details.tabId);
+    }
+}
+var filter = {
+    url: [{
+        pathContains: 'wp-admin'
+    }]
+};
+chrome.webNavigation.onCommitted.addListener(onWebNav, filter);
+chrome.webNavigation.onHistoryStateUpdated.addListener(onWebNav, filter);
